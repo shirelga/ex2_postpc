bc07649103f4785a7e1c66fb0c2be1e123f404b0
shirelga@gmail.com <shirelga@gmail.com>
2017-03-21 01:02:58 +0200

    FAB, dynamic add, color

---
+++ ToDoListManger/app/src/main/java/postpc/todolistmanger/RvAdapter.java
@@ -0,0 +1,72 @@
+package postpc.todolistmanger;
+
+import android.graphics.Color;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.List;
+
+/**
+ * Created by shirelga on 20/03/2017.
+ */
+
+public class RvAdapter extends RecyclerView.Adapter<RvAdapter.ViewHolder>{
+
+    private List<String> mDataset;
+
+    // Provide a reference to the views for each data item
+    // Complex data items may need more than one view per item, and
+    // you provide access to all the views for a data item in a view holder
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        // each data item is just a string in this case
+        public TextView mTextView;
+        public ViewHolder(TextView v) {
+            super(v);
+            mTextView = v;
+        }
+    }
+
+    // Provide a suitable constructor (depends on the kind of dataset)
+    public RvAdapter(List<String> myDataset) {
+        mDataset = myDataset;
+    }
+
+    // Create new views (invoked by the layout manager)
+    @Override
+    public RvAdapter.ViewHolder onCreateViewHolder(ViewGroup parent,
+                                                   int position) {
+        // create a new view
+        TextView v = (TextView) LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.text_layout, parent, false);
+        // set the view's size, margins, paddings and layout parameters
+        ViewHolder vh = new ViewHolder(v);
+        return vh;
+    }
+
+    // Replace the contents of a view (invoked by the layout manager)
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        // - get element from your dataset at this position
+        // - replace the contents of the view with that element
+        Log.i("-I-", mDataset.get(position));
+        holder.mTextView.setText(mDataset.get(position));
+        if((position % 2) == 0)
+        {
+            holder.mTextView.setBackgroundColor(Color.RED);
+        }
+        else
+        {
+            holder.mTextView.setBackgroundColor(Color.BLUE);
+
+        }
+    }
+
+    // Return the size of your dataset (invoked by the layout manager)
+    @Override
+    public int getItemCount() {
+        return mDataset.size();
+    }
+}
